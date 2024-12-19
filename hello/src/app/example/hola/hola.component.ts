394082948b200803f3f505ca85bc625f8fa66563
miguelanguai <miguelangelguaivi@gmail.com>
2024-12-19 15:32:30 +0100

    feat: llamadas al service para traer los sensores del backend

--- hello/src/app/example/hola/hola.component.ts
+++ hello/src/app/example/hola/hola.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HelloWorldService } from '../hello-world.service';
+import { Sensor } from '../sensor';
+import { NgIf, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-hola',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    NgFor
+  ],
   templateUrl: './hola.component.html',
   styleUrl: './hola.component.css'
 })
-export class HolaComponent {
+export class HolaComponent implements OnInit {
 
+
+  sensores: any[] = [];  // Array para almacenar los sensores
+
+  constructor(private helloWorldService: HelloWorldService) {}
+
+  ngOnInit(): void {
+    this.fetchSensores();  // Llamamos al método fetchSensores cuando el componente se inicializa
+  }
+
+  // Método para obtener los sensores del servicio
+  fetchSensores(): void {
+    this.helloWorldService.getSensores().subscribe(
+      (data: Sensor[]) => {
+        this.sensores = data;
+      },
+      (error) => {
+        console.error('Error al obtener sensores:', error);
+      }
+    );
+  }
 }
